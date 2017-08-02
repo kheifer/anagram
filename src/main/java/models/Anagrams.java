79639dd9535e3de2fd9859bae645816f64f41cfd
Guest User <Guest@Epicodus-6E.local>
2017-08-01 17:31:06 -0700

    add initial test

---
+++ src/main/java/models/Anagrams.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by Guest on 8/1/17.
+ */
+public class Anagrams {
+    public String anagramCheck(String first, String second ){
+        return null;
+    }
+
+}
---
+++ src/test/java/models/AnagramsTest.java
@@ -0,0 +1,17 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/1/17.
+ */
+public class AnagramsTest {
+    @Test
+    public void anagramCheck_returnsScoreForSingleLetter_1() throws Exception {
+        Anagrams testAnagram = new Anagrams();
+        boolean expected = true;
+        assertEquals(expected, testAnagram.anagramCheck("taco","coat"));
+    }
+}
